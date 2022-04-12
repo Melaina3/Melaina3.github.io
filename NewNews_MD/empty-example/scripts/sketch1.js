51e31a486809b8e698005755b96494740fac370c
Melaina3 <distadm@augsburg.edu>
2022-04-11 19:06:57 -0500

    Update final project

---
+++ NewNews_MD/empty-example/scripts/sketch1.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(1800, 1000);
+  noStroke();
+}
+function draw() {
+  var x = mouseX;
+  var y = mouseY;
+  background('red');
+  fill(255, 150);
+  var ix = width - mouseX;  // Inverse X
+  var iy = height - mouseY; // Inverse Y
+  ellipse(x, height/2, y, y);
+  fill(0, 159);
+  ellipse(ix, height/2, iy, iy);
+ellipse(x, height/4, y, y);
+fill(0, 159);
+ellipse(ix, height/4, iy, iy);
+}
